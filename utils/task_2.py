7f2ca94f57e903320fe864ad6b2c3f9bc0c2915e
Ihor Nikolskyi <ihor.nikolskyi@gmail.com>
2024-05-13 14:56:13 +0300

    refactor: task_2 remove redundunt spaces

--- utils/task_2.py
+++ utils/task_2.py
@@ -44,28 +44,27 @@ def get_bfs_tree(G, source):
 
 
 def visualize_dfs_bfs_paths_from_node(G, start_node):
-    
+
     dfs_path = get_dfs_tree(G, source=start_node)
     bfs_path = get_bfs_tree(G, source=start_node)
-    
-    
-    pos = nx.spring_layout(G) 
-    
+
+    pos = nx.spring_layout(G)
+
     plt.figure(figsize=(16, 8))
-    
+
     def draw_path(ax, path, color, title):
         nx.draw_networkx_nodes(G, pos, node_size=200, node_color='lightgrey', ax=ax)
         nx.draw_networkx_edges(G, pos, edgelist=G.edges(), edge_color="gray", ax=ax)
         nx.draw_networkx_labels(G, pos, ax=ax)
         nx.draw_networkx_edges(G, pos, edgelist=path.edges, edge_color=color, width=2, ax=ax)
-        
+
         ax.set_title(title)
         ax.axis('off')
-    
+
     ax1 = plt.subplot(1, 2, 1)
     draw_path(ax1, dfs_path, 'lightblue', f'DFS шлях з точки {start_node}')
 
     ax2 = plt.subplot(1, 2, 2)
     draw_path(ax2, bfs_path, 'lightcoral', f'BFS шлях з точки {start_node}')
-    
+
     plt.show()
