1401319addf97ab648547e41003c29933e422c48
Ihor Nikolskyi <ihor.nikolskyi@gmail.com>
2024-05-13 14:54:44 +0300

    feat: task_2 dfs and bfs tree functions

--- utils/task_2.py
+++ utils/task_2.py
@@ -2,6 +2,7 @@
 Завдання 2. Обхід графа в глибину та в ширину з порівняльною візуалізацією
 """
 
+from collections import deque
 import networkx as nx
 import matplotlib.pyplot as plt
 
@@ -11,10 +12,42 @@ def find_least_central_node(G):
     return min(degree_centrality, key=degree_centrality.get)
 
 
+def get_dfs_tree(G, source):
+    tree = nx.Graph()
+    visited = set()
+
+    def dfs_visit(u):
+        visited.add(u)
+        for v in G.neighbors(u):
+            if v not in visited:
+                tree.add_edge(u, v)
+                dfs_visit(v)
+
+    dfs_visit(source)
+    return tree
+
+
+def get_bfs_tree(G, source):
+    visited = {source}
+    tree = nx.Graph()
+    tree.add_node(source)
+    queue = deque([source])
+
+    while queue:
+        node = queue.popleft()
+        for neighbor in G.neighbors(node):
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.append(neighbor)
+                tree.add_edge(node, neighbor)
+    return tree
+
+
 def visualize_dfs_bfs_paths_from_node(G, start_node):
     
-    dfs_path = nx.dfs_tree(G, source=start_node)
-    bfs_path = nx.bfs_tree(G, source=start_node)
+    dfs_path = get_dfs_tree(G, source=start_node)
+    bfs_path = get_bfs_tree(G, source=start_node)
+    
     
     pos = nx.spring_layout(G) 
     
