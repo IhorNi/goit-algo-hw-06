7f17fd5f34ed377188821c2d67020d6f2d3410e2
Ihor Nikolskyi <ihor.nikolskyi@gmail.com>
2024-05-13 15:41:17 +0300

    feat: task_3 add dijkstra path function

--- utils/task_3.py
+++ utils/task_3.py
@@ -2,13 +2,33 @@
 Завдання 3: Вивід найкоротших шляхів з заданої верешини
 """
 
-import networkx as nx
+from queue import PriorityQueue
 from prettytable import PrettyTable
 
 
+def single_source_dijkstra_path(G, source):
+    shortest_paths = {source: [source]}
+    distances = {node: float('inf') for node in G.nodes}
+    distances[source] = 0
+    # PriorityQueue для зручності в пошуку найближчої невідвіданої вершини
+    pq = PriorityQueue()
+    pq.put((0, source))
+
+    while not pq.empty():
+        current_distance, current_node = pq.get()
+        for neighbor in G.neighbors(current_node):
+            distance = G[current_node][neighbor]['weight']
+            new_distance = current_distance + distance
+            if new_distance < distances[neighbor]:
+                distances[neighbor] = new_distance
+                pq.put((new_distance, neighbor))
+                shortest_paths[neighbor] = shortest_paths[current_node] + [neighbor]
+
+    return shortest_paths, distances
+
+
 def print_shortest_paths_from_node(G, start_node):
-    shortest_paths = nx.single_source_dijkstra_path(G, source=start_node)
-    shortest_path_lengths = nx.single_source_dijkstra_path_length(G, source=start_node)
+    shortest_paths, shortest_path_lengths = single_source_dijkstra_path(G, start_node)
 
     table = PrettyTable()
     table.field_names = ["Кінцева вершина", "Найкоротший шлях", "Довжина шляху"]
